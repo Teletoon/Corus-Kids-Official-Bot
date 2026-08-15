de74953f299e239181b2f83c830d51341c9ecb13
Teletoon <younesla2011@hotmail.com>
2026-08-15 11:18:18 -0400

    fixing crash

--- bot.py
+++ bot.py
@@ -209,15 +209,15 @@ async def edit_message(
             )
             return
 
-	allowed_mentions = discord.AllowedMentions(
+        allowed_mentions = discord.AllowedMentions(
             users=True,
             roles=False,
             everyone=False
         )
 
         await message.edit(
-           content=text,
-           allowed_mentions=allowed_mentions
+            content=text,
+            allowed_mentions=allowed_mentions
         )
 
         await interaction.response.send_message(
